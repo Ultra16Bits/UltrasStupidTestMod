bc3ad0384285be934c0c87af30935a1784c80fc8
Ryan <0000727606@senaimgaluno.com.br>
2026-09-18 22:26:32 -0300

    Termino da criação do item "Bath Salts"

--- src/main/java/ultra/testmod/net/item/ModItems.java
+++ src/main/java/ultra/testmod/net/item/ModItems.java
@@ -2,28 +2,27 @@ package ultra.testmod.net.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.item.Item;
 import ultra.testmod.net.TestMod;
-
-import java.util.function.Function;
+import net.minecraft.component.type.ConsumableComponent;
 
 public class ModItems {
-    public static final Item SALTS = registerItem("salts", Item::new, new Item.Settings());
+    public static final Item BATH_SALTS = registerItem("bath_salts", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM,Identifier.of(TestMod.MOD_ID,"bath_salts"))).food(ModFoodComponents.BATH_SALTS,ModConsumableComponents.BATH_SALTS)));
 
-    public static Item registerItem(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
-        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("salts", TestMod.MOD_ID));
-        return Items.register(registryKey,factory,settings);
+    public static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
         TestMod.LOGGER.info("Registering Mod Items for" + TestMod.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
-            entries.add(SALTS);
+            entries.add(BATH_SALTS);
         });
     }
 }
